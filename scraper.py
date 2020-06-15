2fc3926604334c288f6c72270ea032f48a78fece
Tony Pan <tonypan2000@gmail.com>
2020-06-15 19:52:14 -0400

    finds and prints all links from donate buttons

--- scraper.py
+++ scraper.py
@@ -1,33 +1,23 @@
 from bs4 import BeautifulSoup
 import codecs
-import requests
-import urllib
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def fetch_webpage(url):
-    # Some websites disallow urllib requests
-    # urllib2 will require adding a user-agent
-    # but is that allowed/ethically ok to do?
-    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
-
-    # request with urllib
-    request = urllib.request.Request(url, headers=header)
-    response = urllib.request.urlopen(request)
-    html_contents = response.read()
-
-    # request with python requests
-    # res = requests.get(url, headers=header)
-
-    return html_contents
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get(url)
+    html = browser.page_source
+    return html
 
 
 # find and returns all links that the donate buttons point to in a webpage
-def find_donate_button(webpage):
+def find_donate_links(webpage):
     # testing with local site
     # content = codecs.open(webpage, 'r').read()
     # soup = BeautifulSoup(requests.get(url).content)
     soup = BeautifulSoup(webpage, 'html.parser')
-    titles = soup.find_all('a', title='donate')
+    titles = soup.find_all('a', title='Donate')
     links = [entry.get('href') for entry in titles]
     return links
 
@@ -35,4 +25,5 @@ def find_donate_button(webpage):
 def scrape(urls):
     for url in urls:
         webpage = fetch_webpage(url)
-        link = find_donate_button(webpage)
+        links = find_donate_links(webpage)
+        print(links)
